9c3a1f68a3bf5175dcbe3a0dc18038425e43dfc2
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-08-15 10:25:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,22 @@ st.image(image, caption="cool cool")
 
 texto = st.text_input("Hola hola", "este es mi texto")
 st.write("El texto escrito es: ", texto)
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es mi primera columna")
+  st.write("Las interfaces multimodales mejoran la experienci del usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correctooo")
+
+with col2:
+  st.subheader("Esta es mi primera columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual", "Auditiva", "Tactil"))
+  if modo == "Visual":
+    st.write("La vista es fundamental para tu interfaz")
+  if modo == "Auditiva":
+    st.write("La audicion es fundamental para tu interfaz")
+  if modo == "Tactil":
+    st.write("El tacto es fundamental")
