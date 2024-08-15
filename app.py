c39a1a2681d2a253e20d5d166866188327d495a8
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-08-15 10:16:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ st.header ("Hola, como va todo?")
 st.write("Diferente tipo de letra")
 image = Image.open("image1.jpeg")
 st.image(image, caption="cool cool")
+
+texto = st.text_input("Hola hola", "este es mi texto")
+st.write("El texto escrito es: ", texto)
