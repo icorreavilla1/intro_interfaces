bd8284d2fa485ddee057c435c756ea8932385b94
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-08-15 10:06:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from PIL import Image
+
 st.title("MY FIRST APP")
 st.header ("Hola, como va todo?")
 st.write("Diferente tipo de letra")
+image = Image.open("image1.jpeg")
+set.image(image, caption="cool cool :)")
